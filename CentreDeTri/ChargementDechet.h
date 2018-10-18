bbe14f89c278ea1f7439197b19027ec529f58bfd
RemiF <superteam1nsa@robot2.me>
2018-10-18 17:48:31 -0400

    chargement dechet operateur par copie

--- CentreDeTri/ChargementDechet.h
+++ CentreDeTri/ChargementDechet.h
@@ -8,13 +8,17 @@ class ChargementDechet
 public:
 	ChargementDechet(std::list<Dechet*>* l) :liste_dechets(*l) { Compteur::ajouterConstructeur(); }
 	Dechet* getDechet();
-
+	ChargementDechet(ChargementDechet& c);
 	~ChargementDechet();
 
 private:
 	std::list<Dechet*> liste_dechets;
 };
-
+ChargementDechet::ChargementDechet(ChargementDechet& c) {
+	for (list<Dechet*>::iterator it = c.liste_dechets.begin(); it != c.liste_dechets.end(); ++it) {
+		liste_dechets.push_back(new Dechet(**it));
+	}
+}
 ChargementDechet::~ChargementDechet() {
 	for (list<Dechet*>::iterator it = liste_dechets.begin(); it != liste_dechets.end(); ++it) {
 		delete *it;
