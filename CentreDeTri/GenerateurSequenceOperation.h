35a8922934655cf3721e4b13738f73ae812b9049
RemiF <superteam1nsa@robot2.me>
2018-10-25 13:21:55 -0400

    generateur sequence opereation

--- CentreDeTri/GenerateurSequenceOperation.h
+++ CentreDeTri/GenerateurSequenceOperation.h
@@ -1,6 +1,9 @@
 ﻿#pragma once
 #include "UsineTraitement.h"
 #include "SequenceOperations.h"
+#include "CreationDechetRecyclable.h"
+#include "CreationDechetTraiteCompostable.h"
+#include "CreationDechetTraiteNonRecyclable.h"
 #include"Operation1.h"
 #include"Operation2.h"
 #include"Operation3.h"
@@ -29,13 +32,10 @@ SequenceOperations* GenerateurSequenceOperation::genererSequence0(UsineTraitemen
 {
 	//on crée la séquence 0
 	SequenceOperations* seq = new SequenceOperations();
-	//double chainage création ?
-	Operation *op = new Operation(new Operation1(new Operation2(new Operation3(new Operation4(new Operation5(NULL, NULL), NULL), NULL), NULL), NULL), NULL);
-	/*Operation1 *op1 = new Operation1(NULL, NULL);
-	Operation2 *op2 = new Operation2(NULL, op1);
-	Operation3 *op3 = new Operation3(NULL, op2);
-	Operation2 *op2 = new Operation2(NULL, op1);
-	Operation2 *op2 = new Operation2(NULL, op1);*/
-	seq->ajouterOperation(op);
+	//double chainage création ?=non cf schéma
+	//Operation *op = new Operation1(new Operation1(new Operation2(new Operation3(new Operation4(new Operation5(NULL, NULL), NULL), NULL), NULL), NULL), NULL);
+	Operation *op = new Operation1(new CreationDechetTraiteNonRecyclable(usineTraitement),
+		new Operation2(new Operation6(), )
+		seq->ajouterOperation(op);
 	return seq;
 }
